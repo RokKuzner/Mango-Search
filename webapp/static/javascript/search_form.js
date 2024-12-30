5a3bb31e55fedf59f4080d952e8e4dc862166eea
Rok Kuzner <rok.ankaran@gmail.com>
2024-12-30 21:18:30 +0100

    redirect to search page when user submits the form

---
+++ webapp/static/javascript/search_form.js
@@ -0,0 +1,25 @@
+const search_form = document.querySelector("#search-form")
+const search_texarea = document.querySelector("#search-form .searchbox-wrapper .search-textarea-wrapper textarea")
+
+function handle_search() {
+    let query = encodeURIComponent(search_texarea.value)
+    let new_url = window.location.origin + "/search?q=" + query
+    console.log(new_url)
+    window.location.replace(new_url)
+}
+
+search_form.addEventListener("submit", (e)=> {
+    e.preventDefault()
+    handle_search()
+})
+
+document.querySelector("#search-form .searchbox-wrapper .search-icon img").addEventListener("click", ()=> {
+    handle_search()
+})
+
+search_texarea.addEventListener("keydown", (e) => {
+    if (e.key === "Enter" && !e.shiftKey) {
+        e.preventDefault() // Prevent adding a new line in the textarea
+        handle_search()
+    }
+});
