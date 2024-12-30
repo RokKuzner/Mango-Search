5e94b8ad168d04ed6f5990fc2e5c2b4478d4dcae
Rok Kuzner <rok.ankaran@gmail.com>
2024-12-30 20:44:09 +0100

    created view for search

--- webapp/app/urls.py
+++ webapp/app/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
   path('', views.home, name='home'),
+  path('search', views.search, name='search'),
 ]
--- webapp/app/views.py
+++ webapp/app/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
 
 # Create your views here.
 def home(request):
     return render(request, "app/index.html")
+
+def search(request):
+    query = request.GET.get("q", None)
+
+    if query == None:
+        return redirect("/")
+
+    return JsonResponse({"status":"success", "provided query":query})
