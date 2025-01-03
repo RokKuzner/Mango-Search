92409a6a64af470d15e8baa18438ec4ad57cdaf6
Rok Kuzner <rok.ankaran@gmail.com>
2025-01-03 17:24:31 +0100

    created the plan for the search algorithm

---
+++ api/search.py
@@ -0,0 +1,16 @@
+class Search:
+    def __init__(self):
+        pass
+
+    def search(query:str) -> list[str]:
+        # Extract keywords from the query
+        # For each query keyword
+            # Get query keyword features
+            # Select keywords from the database with similar features
+            # For each keyword from the db
+                # Calculate the Levenshtein distance
+                # If Levenshtein distance is small enough -> add 1 point to every website with given keyword
+        # Sort the websites by points
+        # Return sorted websites
+
+        pass
