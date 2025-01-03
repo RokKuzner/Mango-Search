a36e3a913d57f620559efaf0f66cb405a0bf5493
Rok Kuzner <rok.ankaran@gmail.com>
2025-01-03 17:24:58 +0100

    functionto calculate the levenshtein distance

--- api/search.py
+++ api/search.py
@@ -2,6 +2,28 @@ class Search:
     def __init__(self):
         pass
 
+    def levenshtein_distance(self, str1, str2, m, n):
+        # str1 is empty
+        if m == 0:
+            return n
+        
+        # str2 is empty
+        if n == 0:
+            return m
+        
+        if str1[m - 1] == str2[n - 1]:
+            return self.levenshtein_distance(str1, str2, m - 1, n - 1)
+        
+        return 1 + min(
+            # Insert     
+            self.levenshtein_distance(str1, str2, m, n - 1),
+            min(
+                # Remove
+                self.levenshtein_distance(str1, str2, m - 1, n),
+            # Replace
+                self.levenshtein_distance(str1, str2, m - 1, n - 1))
+        )
+
     def search(query:str) -> list[str]:
         # Extract keywords from the query
         # For each query keyword
