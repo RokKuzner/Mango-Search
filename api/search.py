353d49e5d8f14cf2088d51e1234f8aa396476ad9
Rok Kuzner <rok.ankaran@gmail.com>
2025-01-30 15:40:31 +0100

    added comments to search algorithm

--- api/search.py
+++ api/search.py
@@ -11,18 +11,22 @@ def extract_keywords(query:str) -> list[str]:
 
 
 def search(query:str) -> list[str]:
+    # Extract the keywords from the user query
     keywords = extract_keywords(query)
+
     graded_websites = dict()
 
     for query_keyword in keywords:
         websites = get_websites_by_similar_keyyowrds(query_keyword, treshold=0.4)
 
         for website in websites:
+            # Add the similarity between the query keyword and website keyword to website score
             if website["url"] in graded_websites:
                 graded_websites[website["url"]] += website["similarity"]
             else:
                 graded_websites[website["url"]] = website["similarity"]
 
+    # Sort the websites by score
     sorted_websites = sorted(graded_websites.items(), key=lambda item: item[1], reverse=True)
 
     return [url for url, score in sorted_websites]
