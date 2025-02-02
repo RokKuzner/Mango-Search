8172eb7e4de9059729279ae300d7984bcb1f6f96
Rok Kuzner <rok.ankaran@gmail.com>
2025-02-02 16:12:51 +0100

    add request website indexing view and URL pattern

--- webapp/developer/urls.py
+++ webapp/developer/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('request-website-indexing', views.request_website_indexing, name='request website indexing'),
 ]
--- webapp/developer/views.py
+++ webapp/developer/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, "developer/dev_index.html")
+
+def request_website_indexing(request):
+    return render(request, "developer/request_website_indexing.html")
