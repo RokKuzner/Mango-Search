89a72fd5b417e2b6035dabfb2dd5876fcdb7bf44
Rok Kuzner <rok.ankaran@gmail.com>
2025-02-08 17:53:20 +0100

    feat: add endpoint to retrieve last website index time and corresponding view

--- webapp/developer/urls.py
+++ webapp/developer/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.home, name='home'),
     path('request-website-indexing', views.request_website_indexing, name='request website indexing'),
+    path('get-last-website-index-time', views.get_last_website_index_time, name='get last website index time'),
 ]
--- webapp/developer/views.py
+++ webapp/developer/views.py
@@ -3,6 +3,7 @@ import requests
 
 api_url = "http://api:5000"
 api_request_website_index_url = api_url + "/request_website_index"
+api_get_last_website_index_time = api_url + "/get_last_website_index_time"
 
 # Create your views here.
 def home(request):
@@ -32,3 +33,28 @@ def request_website_indexing(request):
         return render(request, "developer/request_website_indexing.html", {"request_success": request_success, "request_message": request_message})
     elif request.method == "GET":
         return render(request, "developer/request_website_indexing.html", {"request_success": None})
+    
+def get_last_website_index_time(request):
+    request_success = None
+    request_message = ""
+
+    if request.method == "POST":
+        post_data =  request.POST.dict()
+
+        if "url" not in post_data:
+            return render(request, "developer/get_last_website_index_time.html", {"request_success": False, "request_message": "URL was not provided"})
+        
+        # Request website index time via the api
+        res = requests.post(api_get_last_website_index_time, json={"url":str(post_data["url"])})
+        res_json = res.json()
+
+        if res.status_code != 200:
+            request_message = res_json["display_msg"]
+            request_success = False
+        else:
+            request_message = f"Your website was indexed on: {res_json["index_time"]}"
+            request_success = True
+        
+        return render(request, "developer/get_last_website_index_time.html", {"request_success": request_success, "request_message": request_message})
+    elif request.method == "GET":
+        return render(request, "developer/get_last_website_index_time.html", {"request_success": None})
